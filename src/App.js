1f93193c9ddd24b5e7ea910dd31d3524f89b3d95
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-08-31 16:42:31 -0500

    Add Greeting component to connect to Rails server
    modified:   src/App.js
    new file:   src/features/greeting/greeting.jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import Counter from './features/counter/Counter';
 import './App.css';
+import Greeting from './features/greeting/greeting';
 
 function App() {
   return (
     <div className="App">
+      <Greeting />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
