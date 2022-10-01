bc370cedba5bd151f48eabf55b428fe8c798099c
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-09-30 21:41:44 -0500

    Change logout to just destroy session and reload window modified:   src/pages/logOut.js

--- src/pages/logOut.js
+++ src/pages/logOut.js
@@ -1,12 +1,9 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
 import { destroySession } from '../services/getUser';
 
 const Logout = () => {
-  const navigate = useNavigate();
   useEffect(() => {
     destroySession();
-    navigate('/');
     window.location.reload();
   });
   return (
